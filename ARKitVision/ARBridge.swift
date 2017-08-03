a4fb424f804f3ba5afdc22e1a444013e69362e4d
mludowise <mludowise@gmail.com>
2017-08-03 13:40:08 -0700

    Label objects in the world

---
+++ ARKitVision/ARBridge.swift
@@ -0,0 +1,18 @@
+//
+//  ARBridge.swift
+//  ARKitVision
+//
+//  Created by Melissa Ludowise on 8/3/17.
+//  Copyright © 2017 Mel Ludowise. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ARBridge {
+    
+    static let shared = ARBridge()
+    
+    var anchorsToIdentifiers = [ARAnchor : String]()
+    
+}
